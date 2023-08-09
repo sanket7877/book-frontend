85f4ab52c503fbb614f2c47cd9ce167a09bfc27d
sanket <lakhtriyasanket@gmail.com>
2023-08-10 00:26:46 +0530

    Update AuthorForm.jsx

--- src/components/AuthorForm.jsx
+++ src/components/AuthorForm.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-
+import { Outlet, Link,useNavigate } from "react-router-dom";
 function AuthorForm() {
 
   const [authorId, setAuthorId] = useState('');
   const [authorName, setAuthorName] = useState('');
-
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     
     e.preventDefault();
@@ -28,6 +28,7 @@ function AuthorForm() {
             alert("Author Added");
             setAuthorId('');
             setAuthorName('');
+            navigate('/');
          } else {
           console.error('Failed to add author');
         }
@@ -41,25 +42,27 @@ function AuthorForm() {
     <div>
       <h2>Add Author</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="authorId">Author ID:</label>
+      <div class="col-8">
+          <label >Author ID:</label>
           <input
             type="text"
             id="authorId"
+            class="form-control"
             value={authorId}
             onChange={(e) => setAuthorId(e.target.value)}
           />
         </div>
-        <div>
-          <label htmlFor="authorName">Author Name:</label>
+        <div class="col-8">
+          <label>Author Name:</label>
           <input
             type="text"
             id="authorName"
+            class="form-control"
             value={authorName}
             onChange={(e) => setAuthorName(e.target.value)}
           />
         </div>
-        <button type="submit">Submit</button>
+        <button class="btn btn-primary" type="submit">Submit</button>
       </form>
     </div>
   );
